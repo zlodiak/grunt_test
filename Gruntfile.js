1516293abe97826e86d41451ef38796a3bcce233
Your Name <you@example.com>
2014-12-28 22:23:10 +0300

    cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,47 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.initConfig({
+        grunt.initConfig({
+            less: {
+              all: {
+                files: {
+                  'css/less.css': 'less/styles.less',
+                }
+              }
+        },
 
-    less: {
-      all: {
-        files: {
-          'css/less.css': 'less/styles.less',
-        }
-      }
-    },
+        watch: {
+            scripts: {
+              files: ['less/styles.less'],  // when file changes
+              tasks: ['less'],            // will start task
+              options: {
+                spawn: false,
+              },
+            },
+        },    
 
-  });
+        // specifying the settings for css file minification
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              cwd: 'css',           // catalog source
+              src: ['less.css'],    // filename
+              dest: 'css',          // catalog destination
+              ext: '.min.css'       //extension add 
+            }]
+          }
+        } 
+    });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', [
     'less',
+    'watch',
+    'cssmin'
   ]);
 
 }
