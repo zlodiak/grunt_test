05755a54bacff37a31be61f6a3340d4d3f71fddd
Black Roland <mail@9241406380.org>
2014-12-29 00:28:39 +0900

    Gruntfile uodate

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,22 @@
 module.exports = function(grunt) {
-  'use strict'; // включить строгий режим
-
-  // автоматически загружать задачи (хотя можно и вручную)
-  require('load-grunt-tasks')(grunt);
-
-  // во избежание «очепяток»
-  // желательно определить директории разработки и деплоя
-  var config = {
-    app: 'app',
-    dist: 'dist'
-  };
+  'use strict';
 
   grunt.initConfig({
-    config: 'config',
 
-    // дальше следует описание задач (см. ниже)
-    less: {},
-    uglify: {},
-    watch: {}
+    less: {
+      all: {
+        files: {
+          'css/less.css': 'less/styles.less',
+        }
+      }
+    },
 
   });
 
-  // «загрузка» задач вручную
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  // регистрация задач, default-задача должна быть обязательно
-  grunt.registerTask('default', ['jshint']);
-};
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', [
+    'less',
+  ]);
+
+}
